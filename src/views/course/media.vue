69bd82d74f1e27531a475bf836cfd004682d22bc
ajiuguniang12136 <1484510552@qq.com>
2021-04-20 16:14:47 +0800

    配置路由

---
+++ src/views/course/media.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        图文
+    </div>
+</template>
+<script>
+import {
+ fetchList,
+ createMedia,
+ updateMedia,
+ deleteMedia
+} from '@/api/media'
+export default ({
+   
+})
+</script>
+
+
+<style scoped>
+
+</style>
