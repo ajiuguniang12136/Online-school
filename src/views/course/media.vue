aff58838f8e92612b605208d1a89d342dc757a5a
ajiu <1484510552@qq.com>
2021-04-21 23:28:01 +0800

    完成图文模块

--- src/views/course/media.vue
+++ src/views/course/media.vue
@@ -1,21 +1,426 @@
 <template>
-    <div>
-        图文
+  <div class="app-container">
+    <div class="filter-container">
+      <el-button class="filter-item" style="margin-left: 10px;" type="primary" icon="el-icon-edit" @click="handleCreate">
+        新增图文
+      </el-button>
+      <div style="float:right">
+        <el-select v-model="listQuery.importance" placeholder="商品状态" clearable style="width: 90px;margin-right:15px;" class="filter-item">
+          <el-option v-for="item in importanceOptions" :key="item" :label="item" :value="item" />
+        </el-select>
+        <el-input v-model="listQuery.title" placeholder="标题" style="width: 200px;" class="filter-item" @keyup.enter.native="handleFilter" />
+        <el-button v-waves class="filter-item" type="primary" icon="el-icon-search" @click="handleFilter">
+          搜索
+        </el-button>
+      </div>
+
     </div>
+
+    <el-table
+      :key="tableKey"
+      v-loading="listLoading"
+      :data="list"
+      border
+      fit
+      highlight-current-row
+      style="width: 100%;"
+      @sort-change="sortChange"
+    >
+      <el-table-column label="ID" prop="id" sortable="custom" align="center" width="80" :class-name="getSortClass('id')">
+        <template slot-scope="{row}">
+          <span>{{ row.id }}</span>
+        </template>
+      </el-table-column>
+      <el-table-column label="图文内容" width="300px">
+        <template slot-scope="{row}">
+          <div style="float:left">
+            <img style="width:100px" :src="row.cover" alt="">
+          </div>
+          <div>
+            <p style="margin:0px">{{ row.title }}</p>
+            <p style="color:red;margin:0px">{{ "￥" + row.price }}</p>
+          </div>
+        </template>
+      </el-table-column>
+      <!-- <el-table-column label="Title" min-width="150px">
+        <template slot-scope="{row}">
+          <span class="link-type" @click="handleUpdate(row)">{{ row.title }}</span>
+          <el-tag>{{ row.type | typeFilter }}</el-tag>
+        </template>
+      </el-table-column> -->
+      <el-table-column label="订阅量" width="110px" align="center">
+        <template slot-scope="{row}">
+          <span>{{ row.sub_count }}</span>
+        </template>
+      </el-table-column>
+      <el-table-column label="状态" class-name="status-col" width="100">
+        <template slot-scope="{row}">
+          <el-tag :type="row.status | statusFilter ">
+            {{ row.status ===1 ? "已上架" : "已下架" }}
+          </el-tag>
+        </template>
+
+      </el-table-column>
+      <!-- <el-table-column v-if="showReviewer" label="Reviewer" width="110px" align="center">
+        <template slot-scope="{row}">
+          <span style="color:red;">{{ row.reviewer }}</span>
+        </template>
+      </el-table-column> -->
+      <!--   <el-table-column label="Imp" width="80px">
+        <template slot-scope="{row}">
+          <svg-icon v-for="n in + row.importance" :key="n" icon-class="star" class="meta-item__icon" />
+        </template>
+      </el-table-column> -->
+      <el-table-column label="创建时间" width="200px" align="center">
+        <template slot-scope="{row}">
+          <span>{{ row.created_time }}</span>
+        </template>
+      </el-table-column>
+      <el-table-column label="操作" align="center" width="230" class-name="small-padding fixed-width">
+        <template slot-scope="{row,$index}">
+          <el-button type="primary" size="mini" @click="handleUpdate(row)">
+            编辑
+          </el-button>
+          <el-button v-if="row.status!='1' " size="mini" type="success" @click="handleModifyStatus(row,1)">
+            上架
+          </el-button>
+          <el-button v-if="row.status!='0' " size="mini" @click="handleModifyStatus(row,0)">
+            下架
+          </el-button>
+          <el-button v-if="row.status!='deleted'" size="mini" type="danger" @click="handleDelete(row,$index)">
+            删除
+          </el-button>
+        </template>
+      </el-table-column>
+      <!--
+      <el-table-column label="Readings" align="center" width="95">
+        <template slot-scope="{row}">
+          <span v-if="row.pageviews" class="link-type" @click="handleFetchPv(row.pageviews)">{{ row.pageviews }}</span>
+          <span v-else>0</span>
+        </template>
+      </el-table-column>
+
+       -->
+    </el-table>
+
+    <pagination v-show="total>0" :total="total" :page.sync="listQuery.page" :limit.sync="listQuery.limit" @pagination="getList" />
+
+    <el-dialog :visible.sync="dialogFormVisible" fullscreen>
+      <p>新增</p>
+      <el-form ref="dataForm" :rules="rules" :model="temp" label-position="left" label-width="80px" style="width: 400px; margin-left:10px;">
+
+        <el-form-item label="标题" prop="title">
+          <el-input v-model="temp.title" />
+        </el-form-item>
+
+        <el-form-item label="封面" prop="cover">
+          <div class="editor-container">
+            <dropzone id="myVueDropzone" v-model="temp.cover" url="https://httpbin.org/post" @dropzone-removedFile="dropzoneR" @dropzone-success="dropzoneS" />
+          </div>
+        </el-form-item>
+
+        <el-form-item label="试看内容" prop="try">
+          <template>
+            <div>
+              <tinymce v-model="temp.try" :width="800" :height="300" />
+            </div>
+          </template>
+        </el-form-item>
+
+        <el-form-item label="课程内容" prop="content">
+          <template>
+            <div>
+              <tinymce v-model="temp.content" :width="800" :height="300" />
+            </div>
+          </template>
+        </el-form-item>
+
+        <el-form-item label="课程价格">
+          <el-input-number v-model="temp.price" />
+        </el-form-item>
+
+        <el-form-item label="状态">
+          <el-radio v-model="temp.status" :label="1">上架</el-radio>
+          <el-radio v-model="temp.status" :label="0">下架</el-radio>
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="dialogFormVisible = false">
+          取消
+        </el-button>
+        <el-button type="primary" @click="dialogStatus==='create'?createData():updateData()">
+          确认
+        </el-button>
+      </div>
+    </el-dialog>
+
+    <el-dialog :visible.sync="dialogPvVisible" title="Reading statistics">
+      <el-table :data="pvData" border fit highlight-current-row style="width: 100%">
+        <el-table-column prop="key" label="Channel" />
+        <el-table-column prop="pv" label="Pv" />
+      </el-table>
+      <span slot="footer" class="dialog-footer">
+        <el-button type="primary" @click="dialogPvVisible = false">Confirm</el-button>
+      </span>
+    </el-dialog>
+  </div>
 </template>
+
 <script>
-import {
- fetchList,
- createMedia,
- updateMedia,
- deleteMedia
-} from '@/api/media'
-export default ({
-   
-})
+import { fetchList, createMedia, updateMedia } from '@/api/media'
+// 上传图片
+import Dropzone from '@/components/Dropzone'
+// 引入富文本编辑器
+import Tinymce from '@/components/Tinymce'
+import waves from '@/directive/waves' // waves directive
+import { parseTime } from '@/utils'
+import { dateFtt } from '../../utils/date'
+import Pagination from '@/components/Pagination' // secondary package based on el-pagination
+
+export default {
+  name: 'Media',
+  components: { Pagination, Dropzone, Tinymce },
+  directives: { waves },
+  filters: {
+    statusFilter(status) {
+      // console.log(status);
+      const statusMap = {
+        1: 'success',
+        0: 'info',
+        deleted: 'danger'
+      }
+      return statusMap[status]
+    }
+
+    // typeFilter(type) {
+    //   return calendarTypeKeyValue[type]
+    // }
+  },
+  data() {
+    return {
+      tableKey: 0,
+      list: null,
+      total: 0,
+      listLoading: true,
+      listQuery: {
+        page: 1,
+        limit: 20,
+        importance: undefined,
+        title: undefined,
+        type: undefined,
+        sort: '+id'
+      },
+      importanceOptions: ['已下架', '已上架'],
+      // calendarTypeOptions,
+      // sortOptions: [{ label: 'ID Ascending', key: '+id' }, { label: 'ID Descending', key: '-id' }],
+      // statusOptions: ['1', '0', 'deleted'],
+      showReviewer: false,
+      temp: {
+        title: '',
+        cover: 'http://dummyimage.com/200x100',
+        try: '',
+        content: '',
+        price: 0,
+        status: 1
+
+      },
+      dialogFormVisible: false,
+      // dialogStatus: '',
+      // textMap: {
+      //   update: 'Edit',
+      //   create: 'Create'
+      // },
+      dialogPvVisible: false,
+      pvData: [],
+      rules: {
+        type: [{ required: true, message: 'type is required', trigger: 'change' }],
+        try: [{ required: true, message: '试看内容不能为空', trigger: 'change' }],
+        title: [{ required: true, message: '标题不能为空', trigger: 'blur' }]
+      },
+      downloadLoading: false
+    }
+  },
+  created() {
+    this.getList()
+  },
+  methods: {
+    dropzoneS(file) {
+      console.log(file)
+      this.$message({ message: 'Upload success', type: 'success' })
+    },
+    dropzoneR(file) {
+      console.log(file)
+      this.$message({ message: 'Delete success', type: 'success' })
+    },
+    getList() {
+      this.listLoading = true
+      fetchList(this.listQuery).then(response => {
+        // console.log(response);
+        this.list = response.data.items
+        console.log(this.list)
+        this.total = response.data.total
+
+        // Just to simulate the time of the request
+        setTimeout(() => {
+          this.listLoading = false
+        }, 1.5 * 1000)
+      })
+    },
+    handleFilter() {
+      this.listQuery.page = 1
+      this.getList()
+    },
+    handleModifyStatus(row, status) {
+      this.$message({
+        message: '操作成功 ',
+        type: 'success'
+      })
+      row.status = status
+      // console.log(row.status);
+    },
+    sortChange(data) {
+      const { prop, order } = data
+      if (prop === 'id') {
+        this.sortByID(order)
+      }
+    },
+    sortByID(order) {
+      if (order === 'ascending') {
+        this.listQuery.sort = '+id'
+      } else {
+        this.listQuery.sort = '-id'
+      }
+      this.handleFilter()
+    },
+    resetTemp() {
+      this.temp = {
+        title: '',
+        cover: 'http://dummyimage.com/200x100',
+        try: '',
+        content: '',
+        price: 0,
+        status: 1
+      }
+    },
+    handleCreate() {
+      this.resetTemp()
+      this.dialogStatus = 'create'
+      this.dialogFormVisible = true
+      this.$nextTick(() => {
+        this.$refs['dataForm'].clearValidate()
+      })
+    },
+    createData() {
+      console.log(this.temp.status)
+      this.$refs['dataForm'].validate((valid) => {
+        if (valid) {
+          this.temp.id = parseInt(Math.random() * 100) + 200 // mock a id
+          this.temp.sub_count = parseInt(Math.random() * 100) + 300 // mock a id
+          this.temp.t_price = 5
+
+          let time = new Date()
+          time = dateFtt('yyyy-MM-dd hh:mm:ss', time)
+          this.temp.updated_time = time
+          this.temp.created_time = time
+
+          console.log(this.temp)
+
+          createMedia(this.temp).then(() => {
+            this.list.unshift(this.temp)
+            this.dialogFormVisible = false
+            this.$notify({
+              title: 'Success',
+              message: '创建成功',
+              type: 'success',
+              duration: 2000
+            })
+          })
+        }
+      })
+    },
+    handleUpdate(row) {
+      this.temp = Object.assign({}, row) // copy obj
+      this.temp.timestamp = new Date(this.temp.timestamp)
+      this.dialogStatus = 'update'
+      this.dialogFormVisible = true
+      this.$nextTick(() => {
+        this.$refs['dataForm'].clearValidate()
+      })
+    },
+    updateData() {
+      this.$refs['dataForm'].validate((valid) => {
+        if (valid) {
+          const tempData = Object.assign({}, this.temp)
+          tempData.timestamp = +new Date(tempData.timestamp) // change Thu Nov 30 2017 16:41:05 GMT+0800 (CST) to 1512031311464
+          updateMedia(tempData).then(() => {
+            const index = this.list.findIndex(v => v.id === this.temp.id)
+            this.list.splice(index, 1, this.temp)
+            this.dialogFormVisible = false
+            this.$notify({
+              title: 'Success',
+              message: '更新成功',
+              type: 'success',
+              duration: 2000
+            })
+          })
+        }
+      })
+    },
+    handleDelete(row, index) {
+      this.$notify({
+        title: 'Success',
+        message: '删除成功',
+        type: 'success',
+        duration: 2000
+      })
+      this.list.splice(index, 1)
+    },
+    // handleFetchPv(pv) {
+    //   fetchPv(pv).then(response => {
+    //     this.pvData = response.data.pvData
+    //     this.dialogPvVisible = true
+    //   })
+    // },
+    handleDownload() {
+      this.downloadLoading = true
+      import('@/vendor/Export2Excel').then(excel => {
+        const tHeader = ['timestamp', 'title', 'type', 'importance', 'status']
+        const filterVal = ['timestamp', 'title', 'type', 'importance', 'status']
+        const data = this.formatJson(filterVal)
+        excel.export_json_to_excel({
+          header: tHeader,
+          data,
+          filename: 'table-list'
+        })
+        this.downloadLoading = false
+      })
+    },
+    formatJson(filterVal) {
+      return this.list.map(v => filterVal.map(j => {
+        if (j === 'timestamp') {
+          return parseTime(v[j])
+        } else {
+          return v[j]
+        }
+      }))
+    },
+    getSortClass: function(key) {
+      const sort = this.listQuery.sort
+      return sort === `+${key}` ? 'ascending' : 'descending'
+    }
+  }
+}
 </script>
 
-
 <style scoped>
+.editor-container{
+    width: 150px;
+    height: 150px;
+    border: 1px dashed #8c939d;
+    border-radius: 6px;
+    cursor: pointer;
+    position: relative;
+   overflow: hidden;
+}
 
 </style>
